16b95bb5e92ab1f535e93da7af105542846215e6
Paul-Erwan RIO <paulerwan.rio@proton.me>
2024-05-18 15:22:02 +0200

    Scan: use Maven API to scan a ISBN barcode
    Restrict scan to EAN-13 format only, and if the data read is not a valid ISBN, try again.

--- app/src/main/java/com/thalyum/isbnsimplescanner/MainActivity.kt
+++ app/src/main/java/com/thalyum/isbnsimplescanner/MainActivity.kt
@@ -2,7 +2,14 @@ package com.thalyum.isbnsimplescanner
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.BaseTransientBottomBar
+import com.google.android.material.snackbar.Snackbar
+import com.google.mlkit.vision.barcode.common.Barcode
+import com.google.mlkit.vision.codescanner.GmsBarcodeScannerOptions
+import com.google.mlkit.vision.codescanner.GmsBarcodeScanning
 import com.thalyum.isbnsimplescanner.databinding.ActivityMainBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,5 +20,45 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.ScanOnceBtn.setOnClickListener {
+            requestScan()
+        }
+    }
+
+    private fun requestScan() {
+        // Setup barcode scanner options
+        val options = GmsBarcodeScannerOptions
+            .Builder()
+            // Only read EAN_13 firmat
+            .setBarcodeFormats(
+                Barcode.FORMAT_EAN_13
+            )
+            // Enable automatic zoom
+            .enableAutoZoom()
+            .build()
+
+        // Get scanner client with custom options
+        val scanner = GmsBarcodeScanning.getClient(this, options)
+
+        scanner.startScan()
+            .addOnSuccessListener { barcode ->
+                // If the result is a valid ISBN, display the value
+                if (barcode.valueType == Barcode.TYPE_ISBN) {
+                    Snackbar.make(binding.root, barcode.rawValue.toString(), Snackbar.LENGTH_SHORT)
+                        .show()
+                } else {
+                    // otherwise, notice the user that it is not a valid ISBN
+                    Snackbar.make(binding.root, R.string.not_isbn, Snackbar.LENGTH_SHORT)
+                        .addCallback(object: BaseTransientBottomBar.BaseCallback<Snackbar>() {
+                            // once the notification is dismissed, scan again
+                            override fun onDismissed(transientBottomBar: Snackbar?, event: Int) {
+                                super.onDismissed(transientBottomBar, event)
+                                requestScan()
+                            }
+                        })
+                        .show()
+                }
+            }
     }
 }
